e38dab7d8acb8f32742a6fc7a89a67f8d58545a6
LolEftRoLua <119731985+loleftroLua@users.noreply.github.com>
2022-12-04 18:16:16 +0700

    Delete New Text Document (4).lua

--- New Text Document (4).lua
+++
@@ -1,7 +0,0 @@
-if game.PlaceId == 6516141723 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/loleftroLua/loleftroGUI/main/DOORS%20SUMMON%20SCRIPT%20V3.lua"))();
-elseif game.PlaceId == 10277607801 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/loleftroLua/loleftroGUI/main/Untitled-1.lua"))();
-elseif game.PlaceId == 6243699076 then
-   loadstring(game:HttpGet("https://github.com/loleftroLua/loleftroGUI/blob/main/THE%20MIMIC%20SUMMON%20SCRIPT%20V1.lua"))();
-end
